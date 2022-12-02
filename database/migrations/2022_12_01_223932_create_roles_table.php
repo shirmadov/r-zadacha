7704ee73ca02b2e30d76667efa61e373b4305db9
Sapa <sapashirmadov2501@gmail.com>
2022-12-02 18:10:43 +0300

    Административная панель

---
+++ database/migrations/2022_12_01_223932_create_roles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('roles')){
+            Schema::create('roles', function (Blueprint $table) {
+                $table->id();
+                $table->string('role_name');
+                $table->timestamps();
+            });
+        }
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role_id')->default(2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_id');
+        });
+    }
+};
